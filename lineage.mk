2f629c77a0ebcbcd390d9b80ca778648d200abe0
BenJule <BenJule@users.noreply.github.com>
2017-10-27 22:50:42 +0200

    Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -7,15 +7,15 @@ TARGET_SCREEN_WIDTH := 1440
 # Enhanced NFC
 # $(call inherit-product, vendor/lineage/config/nfc_enhanced.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit common product files.
+$(call inherit-product, vendor/xenonhd/config/common_full_phone.mk)
 
 # Inherit properties for TeamNexus-bases ROMs
 $(call inherit-product, vendor/nexus/product.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME="zerofltexx" \
-    TARGET_DEVICE="zeroflte"
+    PRODUCT_NAME="Galaxy S6" \
+    TARGET_DEVICE="Galaxy S6"
 
-PRODUCT_NAME := lineage_zerofltexx
+PRODUCT_NAME := xenonhd_jfltexx
 PRODUCT_DEVICE := zerofltexx
