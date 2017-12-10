a5e90573fb467ec1c207dd452de2230d254490d1
BenJule <benlue@s3root.ovh>
2017-12-10 17:37:08 +0100

    Revert "Clean up device tree"
    This reverts commit 3af7e2c7832e56208e078a329c5e6c2592141acd, reversing
    changes made to 56fad7e4c9286a3fe7f65e67be81f7ccf33175fd.

--- xenonhd.mk
+++ xenonhd.mk
@@ -13,24 +13,11 @@ TARGET_SCREEN_WIDTH := 1440
 # Inherit common product files.
 $(call inherit-product, vendor/xenonhd/config/common_full_phone.mk)
 
-<<<<<<< HEAD
 PRODUCT_NAME := xenonhd_zerofltexx
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_DEVICE := zerofltexx
 PRODUCT_MODEL := SM-G920F
-=======
-# Set those variables here to overwrite the inherited values.
-BOARD_VENDOR := Samsung
-PRODUCT_BRAND := Samsung
-PRODUCT_DEVICE := zerofltexx
-PRODUCT_NAME := xenonhd_zerofltexx
-PRODUCT_MANUFACTURER := Samsung
-PRODUCT_MODEL := SM-G920F
-
-# Inherit properties for TeamNexus-bases ROMs
-#$(call inherit-product, vendor/nexus/product.mk)
->>>>>>> 3339252c29961fa0c88af0b8040f88d5ef9df726
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 PRODUCT_SKIP_FINGERPRINT_FROM_FILE := true
@@ -53,14 +40,6 @@ PRODUCT_PACKAGES += \
 
 # Use the latest approved GMS identifiers
 PRODUCT_BUILD_PROP_OVERRIDES += \
-<<<<<<< HEAD
     PRODUCT_NAME=zerofltexx \
     BUILD_FINGERPRINT=samsung/zerofltexx/zerofltexx:8.0.0/OPR3.170623.013/abd222ec54:user/release-keys \
     PRIVATE_BUILD_DESC="zerofltexx-user 8.0.0 OPR3.170623.013 abd222ec54 release-keys"
-=======
-    PRODUCT_NAME="Galaxy S6" \
-    TARGET_DEVICE="Galaxy S6"
-
-PRODUCT_NAME := xenonhd_zerofltexx
-PRODUCT_DEVICE := zerofltexx
->>>>>>> 3339252c29961fa0c88af0b8040f88d5ef9df726
